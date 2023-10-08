b025b0a6a745f7bfca7b4342dca85827b56ca2f3
Anna Luísa <annalf1401@gmail.com>
2023-10-08 20:15:04 -0300

    Atualização: Testbench

--- encode_tb.v
+++ encode_tb.v
@@ -1,126 +1,264 @@
-`include "encode_display.v"
-`timescale 1ns/1ns
+/*
+Declaração do Modulo
+Codificador
+*/
 
-module encode_display_tb;
+module Codificador (A, B, C, D, ready, reset, S0, S1, S2, S3);
 
-reg S0, S1, S2, S3;
-wire a, b, c, d, e, f, g;
+  // Definir as entradas de 4 bits:
+  input wire A;
+  input wire B;
+  input wire C;
+  input wire D;
+  input wire ready;
+  input wire reset;
 
-display uut (
-    .S0(S0),
-    .S1(S1),
-    .S2(S2),
-    .S3(S3),
-    .a(a),
-    .b(b),
-    .c(c),
-    .d(d),
-    .e(e),
-    .f(f),
-    .g(g)
-  );
+  // Definir as saídas:
+  output reg S0;
+  output reg S1;
+  output reg S2;
+  output reg S3;
 
-initial begin
-    $dumpfile("encode_tb.vcd"); // Dumpfile para visualisacao das formas de onda
-    $dumpvars(0, encode_display_tb);;
+  always @(posedge ready or posedge reset) begin
+    if (reset) begin
+      S0 <= 1'bx;
+      S1 <= 1'bx;
+      S2 <= 1'bx;
+      S3 <= 1'bx;
+    end else begin
+      // Definição das expressões booleanas reduzidas via k-map:
+      S0 <= (~A & ~C & D) | (~B & C & ~D) | (B & D) | (A & B & ~C);
+      // Função do 1º bit descrita por A'C'D + B'CD' + BD + ABC'
 
-  // Casos de teste
-  // Intervalos de 10ns por cada entrada, para facilitar a visualisacao
-  // Imprime o tempo, entrada e saida no terminal
-    #10;
-    S0 = 0; S1 = 0; S2 = 0; S3 = 0;
+      S1 <= (~A & ~B & ~C) | (~A & ~C & D) | (B & C & ~D) | (A & ~D);
+      // Função do 2º bit descrita por A'B'C' + A'C'D + BCD' + AD
 
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h",
-             $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 0; S2 = 0; S3 = 0;
+      S2 <= (~A & B & D) | (A & ~B & ~C) | (A & C & D) | (A & B & ~D);
+      // Função do 3º bit descrita por A'BD + AB'C' + BCD + AB'D
 
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 0; S1 = 1; S2 = 0; S3 = 0;
+      S3 <= (~A & ~C & ~D) | (~B & ~D) | (~A & B & D) | (A & ~B & ~C);
+      // Função do 4º bit descrita por A'C'D' + B'D' + A'BD + AB'C'
+    end
+  end
+endmodule
 
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 1; S2 = 0; S3 = 0;
+`timescale 1ns/100ps
 
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 0; S1 = 0; S2 = 1; S3 = 0;
+module TesteBench();
+	reg a, b, c, d, ready, reset;
+	wire s0, s1, s2, s3;
 
-  //5
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 0; S2 = 1; S3 = 0;
+	Codificador tb(.A(a), .B(b), .C(c), .D(d), .ready(ready), .reset(reset), .S0(s0), .S1(s1), .S2(s2), .S3(s3));
+	
+	initial begin
+		// Inicialização das variáveis
+		// 0;
+		a = 0;
+		b = 0;
+		c = 0;
+		d = 0;
+		ready = 1;
+	
+		// Testando para cada numero
+		// 1;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 0; b = 0; c = 0; d = 1;
+		ready = 1;
+		#1;
 
-   //6
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 0; S1 = 1; S2 = 1; S3 = 0;
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		// 2;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 0; b = 0; c = 1; d = 0;
+		ready = 1;
+		#1
 
-    //7
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 1; S2 = 1; S3 = 0;
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		// 3;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 0; b = 0; c = 1; d = 1;
+		ready = 1;
+		#1
+		
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
 
-     //8
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 0; S1 = 0; S2 = 0; S3 = 1;
+		// 4;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 0; b = 1; c = 0; d = 0;
+		ready = 1;
+		#1
+		
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
 
-     //9
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 0; S2 = 0; S3 = 1;
+		// 5;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 0; b = 1; c = 0; d = 1;
+		ready = 1;
+		#1
+		
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
 
-     //10
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 0; S1 = 1; S2 = 0; S3 = 1;
+		// 6;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 0; b = 1; c = 1; d = 0;
+		ready = 1;
+		#1
+		
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
 
-    //11
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 1; S2 = 0; S3 = 1;
+		// 7;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 0; b = 1; c = 1; d = 1;
+		ready = 1;
+		#1
+		
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
 
-    //12
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 0; S1 = 0; S2 = 1; S3 = 1;
+		// 8;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 0; c = 0; d = 0;
+		ready = 1;
+		#1
 
-    //13
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 0; S2 = 1; S3 = 1;
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		// 9;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 0; c = 0; d = 1;
+		ready = 1;
+		#1
 
-    //14
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 0; S1 = 1; S2 = 1; S3 = 1;
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		// 10;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 0; c = 1; d = 0;
+		ready = 1;
+		#1
 
-    //15
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
-    #10;
-    S0 = 1; S1 = 1; S2 = 1; S3 = 1;
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		// 11;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 0; c = 1; d = 1;
+		ready = 1;
+		#1
 
-    $display("Time = %0t | Entrada = %0h%0h%0h%0h%0h Saída = %0h%0h%0h%0h%0h%0h%0h", 
-    $time, S0, S1, S2, S3, a, b, c, d, e, f, g);
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		// 12;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 1; c = 0; d = 0;
+		ready = 1;
+		#1
+		
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
 
-    #100; // Adicione um atraso maior antes de finalizar para visualizar melhor
-    $finish;
-end
+		// 13;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 1; c = 0; d = 1;
+		ready = 1;
+		#1
 
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		// 14;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 1; c = 1; d = 0;
+		ready = 1;
+		#1
+		
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+
+		// 15;
+		#1;
+		reset = 1;
+		ready = 0;
+		#1;
+		reset = 0;
+		a = 1; b = 1; c = 1; d = 1;
+		ready = 1;
+		#1
+
+        $display("Time = %0t | Entrada = %0d%0d%0d%0d Saída = %0d%0d%0d%0d",
+             $time, a, b, c, d, s0, s1, s2, s3);
+		
+		$dumpfile("teste_gtk.vcd");
+        $dumpvars;
+		
+		// Finalizar a simulação
+		$stop;
+	end
 endmodule
