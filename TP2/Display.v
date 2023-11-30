7af415254f9a6cededa5cc72346b9e219a3ff3cd
Gabriel_Duarte <88411632+gabriel2036@users.noreply.github.com>
2023-11-30 11:57:34 -0300

    Add novos caso Display

--- TP2/Display.v
+++ TP2/Display.v
@@ -16,8 +16,8 @@ module Display(premio_f, p1_f, p2_f, state_f, hex7, hex6, hex5, hex4, ledp1, led
           sete   = 7'b0001111,
           oito   = 7'b0000000,
           nove   = 7'b0000100,
-			 traco  = 7'b1111110, // traço
-          apagado =7'b0000000; // tudo
+		  traco  = 7'b1111110, 
+          apagado =7'b0000000; 
 
 
     always@(*)begin
@@ -26,13 +26,33 @@ module Display(premio_f, p1_f, p2_f, state_f, hex7, hex6, hex5, hex4, ledp1, led
                 ledp1 = 2'b00; 
                 ledp2 = 2'b11;
                 case (p1_f)
+                    0: begin
+                        hex7 = zero;
+                        hex6 = zero;
+                    end
                     1: begin
                         hex7 = zero;
                         hex6 = um;
                     end
                     2: begin
                         hex7 = zero;
-                        hex6 = um;
+                        hex6 = dois;
+                    end
+                    3: begin
+                        hex7 = zero;
+                        hex6 = tres;
+                    end
+                    4: begin
+                        hex7 = zero;
+                        hex6 = quatro;
+                    end
+                    5: begin
+                        hex7 = zero;
+                        hex6 = cinco;
+                    end
+                    default: begin
+                        hex7 = traco;
+                        hex6 = traco;
                     end
                 endcase
             end
@@ -40,9 +60,33 @@ module Display(premio_f, p1_f, p2_f, state_f, hex7, hex6, hex5, hex4, ledp1, led
                 ledp2 = 2'b00; 
                 ledp1 = 2'b11;
                 case (p2_f)
+                    0: begin
+                        hex5 = zero;
+                        hex4 = um;
+                    end
                     1: begin
                         hex5 = zero;
                         hex4 = um;
+                    end
+                    2: begin
+                        hex5 = zero;
+                        hex4 = dois;
+                    end
+                    3: begin
+                        hex5 = zero;
+                        hex4 = tres;
+                    end
+                    4: begin
+                        hex5 = zero;
+                        hex4 = quatro;
+                    end
+                    5: begin
+                        hex5 = zero;
+                        hex4 = cinco;
+                    end
+                    default: begin
+                        hex5 = traco;
+                        hex4 = traco;
                     end
                 endcase
             end 
@@ -50,10 +94,15 @@ module Display(premio_f, p1_f, p2_f, state_f, hex7, hex6, hex5, hex4, ledp1, led
         endcase
         case (state_f)
            
-            4'b0000: begin // acender led vermelho quando tiver no estado s0
+            4'b0000: begin // acender led 15 quando estiver no estado s0
                 led15 = 4'b0000;
             end
-				default: led15 = 4'b1111;
+            4'b1000: begin // acender os leds p1, p2 e 15 quando estiver no estado de espera (sgx)
+                ledp1 = 0;
+                ledp2 = 0;
+                led15 = 4'b0000;
+            end
+			default: led15 = 4'b1111;
         endcase
     end
 endmodule
